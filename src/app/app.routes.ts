22f241c0ba5daa80173c0bc5d97e3cd266a51bc8
nkhairina <nasreenkhairina@gmail.com>
2024-03-03 23:28:36 +0800

    03032024-2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './home/login/login.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
+import { ReferencesComponent } from './home/references/references.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { AdditionalInfoComponent } from './additional-info/additional-info.component';
+import { AddressComponent } from './address/address.component';
+import { EducationComponent } from './education/education.component';
+import { UserdiversityComponent } from './userdiversity/userdiversity.component';
+import { UserdocumentsComponent } from './userdocuments/userdocuments.component';
+import { UserinformationComponent } from './userinformation/userinformation.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'references', component: ReferencesComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'additionalInfo', component: AdditionalInfoComponent },
+  { path: 'address', component: AddressComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'userdiversity', component: UserdiversityComponent },
+  { path: 'userdocuments', component: UserdocumentsComponent },
+  { path: 'userinformation', component: UserinformationComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
