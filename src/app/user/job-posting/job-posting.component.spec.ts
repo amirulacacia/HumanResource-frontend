e712dd547aa90a3175f13a1547fc11293b91d2ba
nkhairina <nasreenkhairina@gmail.com>
2024-03-04 23:01:30 +0800

    update

--- src/app/user/job-posting/job-posting.component.spec.ts
+++ src/app/user/job-posting/job-posting.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PostJobComponent } from './post-job.component';
+import { JobPostingComponent } from './job-posting.component';
 
-describe('PostJobComponent', () => {
-  let component: PostJobComponent;
-  let fixture: ComponentFixture<PostJobComponent>;
+describe('JobPostingComponent', () => {
+  let component: JobPostingComponent;
+  let fixture: ComponentFixture<JobPostingComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PostJobComponent]
+      imports: [JobPostingComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(PostJobComponent);
+    fixture = TestBed.createComponent(JobPostingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
