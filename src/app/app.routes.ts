e06d4858547cd5d8e52f37fdca51d186726b57aa
amirulacacia <amirulacacia@outlook.com>
2024-03-04 15:30:58 +0800

    remove Loginhtml

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { LoginComponent } from './home/login/login.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { HeaderComponent } from './home/header/header.component';
-import { ReferencesComponent } from './home/references/references.component';
-import { ExperienceComponent } from './user_info/experience/experience.component';
-import { AdditionalInfoComponent } from './user_info/additionalinfo/additionalinfo.component';
-import { AddressComponent } from './user_info/address/address.component';
-import { EducationComponent } from './user_info/education/education.component';
-import { UserdiversityComponent } from './user_info/userdiversity/userdiversity.component';
-import { UserdocumentsComponent } from './user_info/userdocuments/userdocuments.component';
-import { UserinformationComponent } from './user_info/userinformation/userinformationpage.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'footer', component: FooterComponent },
   { path: 'header', component: HeaderComponent },
-  { path: 'references', component: ReferencesComponent },
-  { path: 'experience', component: ExperienceComponent },
-  { path: 'additionalInfo', component: AdditionalInfoComponent },
-  { path: 'address', component: AddressComponent },
-  { path: 'education', component: EducationComponent },
-  { path: 'userdiversity', component: UserdiversityComponent },
-  { path: 'userdocuments', component: UserdocumentsComponent },
-  { path: 'userinformation', component: UserinformationComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { Router } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
@@ -10,33 +9,33 @@ import { HeaderComponent } from '../header/header.component';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  @ViewChild('emailInput') emailInput!: ElementRef;
-  @ViewChild('passwordInput') passwordInput!: ElementRef;
-  @ViewChild('jobIdInput') jobIdInput!: ElementRef;
+  // @ViewChild('emailInput') emailInput!: ElementRef;
+  // @ViewChild('passwordInput') passwordInput!: ElementRef;
+  // @ViewChild('jobIdInput') jobIdInput!: ElementRef;
 
-  userType: string = 'applicant'; // Default user type
+  // userType: string = 'applicant'; // Default user type
 
-  constructor(private router: Router) {}
+  // constructor(private router: Router) {}
 
-  login() {
-    const email = this.emailInput.nativeElement.value;
-    const password = this.passwordInput.nativeElement.value;
-    const jobId = this.jobIdInput.nativeElement.value;
+  // login() {
+  //   const email = this.emailInput.nativeElement.value;
+  //   const password = this.passwordInput.nativeElement.value;
+  //   const jobId = this.jobIdInput.nativeElement.value;
 
-    if (this.userType === 'applicant') {
-      if (email && password) {
-        // Perform login logic for applicants (email and password)
-        this.router.navigate(['/applicant-dashboard']);
-      } else {
-        alert('Please enter valid email and password for applicant login.');
-      }
-    } else if (this.userType === 'admin') {
-      if (jobId && password) {
-        // Perform login logic for admins (job_id and password)
-        this.router.navigate(['/admin-dashboard']);
-      } else {
-        alert('Please enter valid job ID and password for admin login.');
-      }
-    }
-  }
+  //   if (this.userType === 'applicant') {
+  //     if (email && password) {
+  //       // Perform login logic for applicants (email and password)
+  //       this.router.navigate(['/applicant-dashboard']);
+  //     } else {
+  //       alert('Please enter valid email and password for applicant login.');
+  //     }
+  //   } else if (this.userType === 'admin') {
+  //     if (jobId && password) {
+  //       // Perform login logic for admins (job_id and password)
+  //       this.router.navigate(['/admin-dashboard']);
+  //     } else {
+  //       alert('Please enter valid job ID and password for admin login.');
+  //     }
+  //   }
+  // }
 }
