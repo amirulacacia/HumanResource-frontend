a07199bb40726ff386dacef5b928a91acc09f8fd
amirulacacia <amirulacacia@outlook.com>
2024-03-05 15:53:50 +0800

    Edit routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,2 +1,15 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './home/login/login.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { SignupComponent } from './home/signup/signup.component';
 
-
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
