9731a412fdc3611f6509e46bad82eab358d90eb8
amirulacacia <amirulacacia@outlook.com>
2024-03-06 02:51:28 +0800

    Still cant do it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, computed, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { SideNavComponent } from "./home/side-nav/side-nav.component";
+import { ContentComponent } from "./home/content/content.component";
 
 @Component({
     selector: 'app-root',
@@ -19,13 +20,15 @@ import { SideNavComponent } from "./home/side-nav/side-nav.component";
         MatIconModule,
         MatButtonModule,
         MatSidenavModule,
-        SideNavComponent
+        SideNavComponent,
+        ContentComponent
     ]
 })
 export class AppComponent {
   title(title: any) {
     throw new Error('Method not implemented.');
   }
+  
   // title = 'hrApplication';
 
   collapsed = signal(false);
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,44 @@ import { ApplicantComponent } from './user/user_info/applicant/applicant.compone
 import { ContentComponent } from './home/content/content.component';
 import { EmployeeLoginComponent } from './home/employee-login/employee-login.component';
 import { ApplicantLoginComponent } from './home/applicant-login/applicant-login.component';
+import { SideNavComponent } from './home/side-nav/side-nav.component';
 
 export const routes: Routes = [
-  { path: 'contact', component: ContactComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'user', component: ViewUserInfoComponent },
-  { path: 'applicant', component: ApplicantComponent },
-  { path: 'home', component: ContentComponent },
-  { path: 'employee-login', component: EmployeeLoginComponent },
-  { path: 'applicant-login', component: ApplicantLoginComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { 
+    path: 'contact', 
+    component: ContactComponent 
+  },
+  { 
+    path: 'signup', 
+    component: SignupComponent 
+  },
+  { 
+    path: 'user', 
+    component: ViewUserInfoComponent 
+  },
+  { 
+    path: 'applicant',
+    component: ApplicantComponent 
+  },
+  { 
+    path: 'home', 
+    component: ContentComponent 
+  },
+  { 
+    path: 'employee-login', 
+    component: EmployeeLoginComponent, 
+  },
+  { 
+    path: 'applicant-login', 
+    component: ApplicantLoginComponent, 
+  },
+  { 
+    path: 'side-bar', 
+    component: SideNavComponent
+  },
+  { 
+    path: '', 
+    redirectTo: '/home', 
+    pathMatch: 'full' 
+  },
 ];
