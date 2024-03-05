78039c65f1f49ff473cd8f3a8b4a100052dee38f
Nazliah-java <nazliahborhan5@gmail.com>
2024-03-05 15:59:09 +0800

    nazliah

---
+++ src/app/view-user-info/view-user-info.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewUserInfoComponent } from './view-user-info.component';
+
+describe('ViewUserInfoComponent', () => {
+  let component: ViewUserInfoComponent;
+  let fixture: ComponentFixture<ViewUserInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ViewUserInfoComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ViewUserInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
