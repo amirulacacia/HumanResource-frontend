8f62c1e8dc40ad740e0ebd640411aec379f792a6
nkhairina <nasreenkhairina@gmail.com>
2024-03-06 01:05:37 +0800

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,8 @@ import { ContactComponent } from './home/contact/contact.component';
 import { SignupComponent } from './home/signup/signup.component';
 import { ViewUserInfoComponent } from './view-user-info/view-user-info.component';
 import { ApplicantComponent } from './user/user_info/applicant/applicant.component';
-import { ContentComponent } from './home/content/content.component';
-import { EmployeeLoginComponent } from './home/employee-login/employee-login.component';
-import { ApplicantLoginComponent } from './home/applicant-login/applicant-login.component';
 import { JoblistingComponent } from './user/joblisting/joblisting.component';
+
 
 
 export const routes: Routes = [
@@ -14,5 +12,6 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'user', component: ViewUserInfoComponent },
   { path: 'applicant', component: ApplicantComponent },
+  { path: 'joblisting', component: JoblistingComponent }
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
