2ebebbdc92c74eecaead5a5264f6a4b23783f18d
Nazliah-java <nazliahborhan5@gmail.com>
2024-03-06 12:06:07 +0800

    interview status

---
+++ src/app/interviewstatus/interviewstatus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interviewstatus',
+  standalone: true,
+  imports: [],
+  templateUrl: './interviewstatus.component.html',
+  styleUrl: './interviewstatus.component.scss'
+})
+export class InterviewstatusComponent {
+
+}
