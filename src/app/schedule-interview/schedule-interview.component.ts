4809d0f2fd7ea37a7ad8f6c117f368334ee46a7c
nkhairina <nasreenkhairina@gmail.com>
2024-03-06 12:28:25 +0800

    add

---
+++ src/app/schedule-interview/schedule-interview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-interview',
+  standalone: true,
+  imports: [],
+  templateUrl: './schedule-interview.component.html',
+  styleUrl: './schedule-interview.component.scss'
+})
+export class ScheduleInterviewComponent {
+
+}
